d62369346370689935ad68db1c970302e7243b60
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-04-22 22:32:12 +0300

    fix generation

--- tests/generate_current_network_status.py
+++ tests/generate_current_network_status.py
@@ -33,41 +33,50 @@ def get_networks_list(folder):
     return sub_folders
 
 
-def get_value_from_subquery(network: str, stage: bool):
-    r = requests.get(
-        'https://api.subquery.network/subqueries/nova-wallet/nova-wallet-%s?stage=%s' % (network, stage))
-    response = json.loads(r.content)
+def get_value_from_subquery(network):
+    project_data = use_subquery_cli(subquery_cli_version, '--token', token, 'deployment', 'list', '--key', 'nova-wallet-'+network, '--org', 'nova-wallet', '-o', 'json')
+    response = json.loads(project_data)
     return response
 
 
-def get_percentage(network, project_id):
-    percentage = use_subquery_cli(subquery_cli_version, '--token', token, 'deployment',
-                                  'sync-status', '--id', str(project_id), '--key', 'nova-wallet-'+network, '--org', 'nova-wallet')
-    status = percentage.split("percent: ")[1:]
-    returning_value = status[0].split('%')[0:][0].split('.')[0]
+def get_percentage(network):
+    try:
+        percentage = use_subquery_cli(subquery_cli_version, '--token', token, 'deployment',
+                                  'sync-status', '--id', str(network['id']), '--key', network['projectKey'].split('/')[1], '--org', 'nova-wallet')
+        status = percentage.split("percent: ")[1:]
+        returning_value = status[0].split('%')[0:][0].split('.')[0]
+    except:
+        returning_value = False
     return returning_value
 
 
-def generate_progress_status(network, stage):
-    data = get_value_from_subquery(network, stage)
-    if (data['deployed']):
-        commit = data['deployment']['version'][0:8]
-        if (stage):
-            if (data['deployment']['healthStatus']['indexer']):
-                percent = get_percentage(network, data['deployment']['id'])
-                progress_bar = '![%s](https://progress-bar.dev/%s?title=Stage)' % (percent, percent)
-            else:
-                progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
-        else:
-            if (data['deployment']['healthStatus']['indexer']):
-                percent = get_percentage(network, data['deployment']['id'])
-                progress_bar = "![%s](https://progress-bar.dev/%s?title=Prod)" % (percent, percent)
-            else:
-                progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
-    else:
-        progress_bar = '![0](https://progress-bar.dev/0?title=Not%20Deployed)'
-        commit = '-'
-    return progress_bar, commit
+def generate_progress_status(network):
+    data = get_value_from_subquery(network)
+    prod_commit = prod_progress_bar = stage_commit = stage_progress_bar = '-'
+
+    for network in data:
+        if (network['type'] == 'primary'):
+            prod_commit = network['version'][0:8]
+            if (network['status'] == 'error'):
+                prod_progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
+            elif (network['status'] == 'running'):
+                prod_percent = get_percentage(network)
+                if (prod_percent):
+                    prod_progress_bar = "![%s](https://progress-bar.dev/%s?title=Prod)" % (prod_percent, prod_percent)
+                else:
+                    prod_progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
+
+        elif (network['type'] == 'stage'):
+            stage_commit = network['version'][0:8]
+            if (network['status'] == 'error'):
+                stage_progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
+            elif (network['status'] == 'running'):
+                stage_percent = get_percentage(network)
+                if (stage_percent):
+                    stage_progress_bar = "![%s](https://progress-bar.dev/%s?title=Stage)" % (stage_percent, stage_percent)
+                else:
+                    stage_progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
+    return prod_progress_bar, prod_commit, stage_commit, stage_progress_bar
 
 
 def generate_value_matrix():
@@ -79,11 +88,10 @@ def generate_value_matrix():
             "[%s](https://explorer.subquery.network/subquery/nova-wallet/nova-wallet-%s)" % (
                 network.title(), network)
         )
-        stage_status, stage_comit = generate_progress_status(network, stage=True)
-        prod_status, prod_commit = generate_progress_status(network, stage=False)
-        network_data_array.append(stage_status)
-        network_data_array.append(prod_status)
-        network_data_array.append(stage_comit)
+        prod_progress_bar, prod_commit, stage_commit, stage_progress_bar = generate_progress_status(network)
+        network_data_array.append(stage_progress_bar)
+        network_data_array.append(prod_progress_bar)
+        network_data_array.append(stage_commit)
         network_data_array.append(prod_commit)
         returning_array.append(network_data_array)
         print('%s generated!'%network.title())
