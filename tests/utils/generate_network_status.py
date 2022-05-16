18f8060473c656c2882f872639c6e25190b440b2
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-05-16 21:40:40 +0300

    fix logic

--- tests/utils/generate_network_status.py
+++ tests/utils/generate_network_status.py
@@ -3,7 +3,6 @@
 
 import json
 import os
-import requests
 from jinja2 import Template
 from pytablewriter import MarkdownTableWriter
 from subquery_cli import use_subquery_cli
@@ -20,7 +19,8 @@ Projects' status is updated every 4 hours
 def generate_networks_list():
     writer = MarkdownTableWriter(
         table_name="List of deployed projects",
-        headers=["Network", "Stage status", "Prod status", "Stage commit", "Prod commit"],
+        headers=["Network", "Stage status",
+                 "Prod status", "Stage commit", "Prod commit"],
         value_matrix=generate_value_matrix(),
         margin=1
     )
@@ -34,41 +34,52 @@ def get_networks_list(folder):
     return sub_folders
 
 
-def get_value_from_subquery(network: str, stage: bool):
-    r = requests.get(
-        'https://api.subquery.network/subqueries/nova-wallet/nova-wallet-%s?stage=%s' % (network, stage))
-    response = json.loads(r.content)
-    return response
+def get_deployments_list(network: str):
+    deployments = use_subquery_cli(subquery_cli_version, '--token', token, 'deployment',
+                                   'list', '--key', 'nova-wallet-'+network, '--org', 'nova-wallet', '-o', 'json')
+    production_instance = None
+    stage_instance = None
+
+    for instance in json.loads(deployments):
+        if (instance.get('type') == 'primary'):
+            production_instance = instance
+        else:
+            stage_instance = instance
+
+    return production_instance, stage_instance
 
 
 def get_percentage(network, project_id):
-    percentage = use_subquery_cli(subquery_cli_version, '--token', token, 'deployment',
+    try:
+        percentage = use_subquery_cli(subquery_cli_version, '--token', token, 'deployment',
                                   'sync-status', '--id', str(project_id), '--key', 'nova-wallet-'+network, '--org', 'nova-wallet')
-    status = percentage.split("percent: ")[1:]
-    returning_value = status[0].split('%')[0:][0].split('.')[0]
-    return returning_value
+        status = percentage.split("percent: ")[1:]
+        return status[0].split('%')[0:][0].split('.')[0]
+    except:
+        return '0'
 
 
-def generate_progress_status(network, stage):
-    data = get_value_from_subquery(network, stage)
-    if (data.get('deployed')):
-        commit = data.get('deployment').get('version')[0:8]
-        if (stage):
-            if (data.get('deployment').get('healthStatus').get('indexer')):
-                percent = get_percentage(network, data['deployment']['id'])
-                progress_bar = '![%s](https://progress-bar.dev/%s?title=Stage)' % (percent, percent)
+def generate_progress_status(network):
+    prod, stage = get_deployments_list(network)
+
+    def fill_status_bar(instance):
+        if (instance):
+            commit = instance.get('version')
+            if (instance.get('status') == 'running'):
+                percent = get_percentage(network, instance.get('id'))
+                progress_bar = '![%s](https://progress-bar.dev/%s?title=%s)' % (
+                    percent, percent, instance.get('type').capitalize())
             else:
                 progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
         else:
-            if (data.get('deployment').get('healthStatus').get('indexer')):
-                percent = get_percentage(network, data['deployment']['id'])
-                progress_bar = "![%s](https://progress-bar.dev/%s?title=Prod)" % (percent, percent)
-            else:
-                progress_bar = '![0](https://progress-bar.dev/0?title=Error)'
-    else:
-        progress_bar = '![0](https://progress-bar.dev/0?title=Not%20Deployed)'
-        commit = '-'
-    return progress_bar, commit
+            progress_bar = '![0](https://progress-bar.dev/0?title=Not%20Deployed)'
+            commit = '-'
+        return progress_bar, commit
+
+    prod_status, prod_commit = fill_status_bar(prod)
+    stage_status, stage_commit = fill_status_bar(stage)
+
+    return prod_status, prod_commit, stage_status, stage_commit
 
 
 def generate_value_matrix():
@@ -80,14 +91,14 @@ def generate_value_matrix():
             "[%s](https://explorer.subquery.network/subquery/nova-wallet/nova-wallet-%s)" % (
                 network.title(), network)
         )
-        stage_status, stage_comit = generate_progress_status(network, stage=True)
-        prod_status, prod_commit = generate_progress_status(network, stage=False)
+        prod_status, prod_commit, stage_status, stage_comit = generate_progress_status(
+            network)
         network_data_array.append(stage_status)
         network_data_array.append(prod_status)
         network_data_array.append(stage_comit)
         network_data_array.append(prod_commit)
         returning_array.append(network_data_array)
-        print('%s generated!'%network.title())
+        print('%s generated!' % network.title())
     returning_array.sort()
     return returning_array
 
@@ -97,9 +108,9 @@ if __name__ == '__main__':
     dir_name = 'gh-pages'
     try:
         os.makedirs(dir_name)
-        print("Directory " , dir_name ,  " Created ")
+        print("Directory ", dir_name,  " Created ")
     except FileExistsError:
-        print("Directory " , dir_name ,  " already exists")
+        print("Directory ", dir_name,  " already exists")
 
     with open("./gh-pages/README.md", "w") as f:
         f.write(readme.render(
